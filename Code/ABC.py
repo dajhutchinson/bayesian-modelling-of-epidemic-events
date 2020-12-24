43a2bc1fc3a4b310de6b315dedb3a9b6be57afdb
Dom Hutchinson <dom@domhutchinson.me>
2020-12-24 00:12:36 +0000

    multi-comparison sampling

--- Code/ABC.py
+++ Code/ABC.py
@@ -1,7 +1,8 @@
 """
-PARAMETERS
-prior for parameters
-Set of observed summary statistic values
+TODO
+ - ABC-SMC
+ - ABC-Semi-Automatic
+ - Summary statistics
 """
 
 from scipy import stats
@@ -19,7 +20,8 @@ def l2_norm(s:(float),s_obs:(float)) -> float:
    PLOTTING
 """
 
-def __plot_results(fig:plt.Figure,model_hat:Model,model_star:Model,samples:[([float],([float],float))],priors:["stats.distribution"],var_ranges:[(float,float)],observations:[([float],float)],plot_truth=True):
+# def __plot_results(fig:plt.Figure,model_hat:Model,model_star:Model,samples:[([float],([float],float))],priors:["stats.distribution"],var_ranges:[(float,float)],observations:[([float],float)],plot_truth=True):
+def __plot_results(fig:plt.Figure,model_hat:Model,model_star:Model,THETA_SAMPLES:[[float]],ACCEPTED_OBS:[([float],float)],priors:["stats.distribution"],var_ranges:[(float,float)],observations:[([float],float)],plot_truth=True):
     """
     DESCRIPTION
     Produces plots for parameter posteriors and model fit (if <=2 variables).
@@ -30,20 +32,19 @@ def __plot_results(fig:plt.Figure,model_hat:Model,model_star:Model,samples:[([fl
 
     # plot parameter results
     for i in range(model_star.n_params):
-        theta_i_samples=[x[0][i] for x in samples]
+        theta_i_samples=[x[i] for x in THETA_SAMPLES]
         ax=fig.add_subplot(1,n_plots,i+1)
         __plot_posterior(ax,"Theta_{}".format(i),model_star.params[i],theta_i_samples,priors[i],plot_truth)
 
-    accepted_samples=[x[1] for x in samples]
     # 2d plot
     if (model_star.n_vars==1):
         ax=fig.add_subplot(1,n_plots,n_plots)
-        __plot_2d_samples(ax,accepted_samples,observations,model_hat,plot_truth)
+        __plot_2d_samples(ax,ACCEPTED_OBS,observations,model_hat,plot_truth)
 
     # 2d plot
     elif (model_star.n_vars==2):
         ax=fig.add_subplot(1,n_plots,n_plots,projection="3d")
-        __plot_3d_samples(ax,accepted_samples,observations,model_hat,model_star,var_ranges,plot_truth)
+        __plot_3d_samples(ax,ACCEPTED_OBS,observations,model_hat,model_star,var_ranges,plot_truth)
 
     plt.get_current_fig_manager().window.state('zoomed')
     plt.show()
@@ -180,19 +181,19 @@ def __plot_3d_samples(ax:plt.Axes,accepted_samples:[([float],float)],observation
     SAMPLING STAGE
 """
 
-def __sample_stage_fixed_sample_size(DESIRED_SAMPLE_SIZE:int,EPSILON:float,x_obs:[[float]],s_obs:[float],PRIORS:["stats.Distribution"],x_samplers:["stats.Distribution"],model_t:Model) -> [([float],float)]:
+def __sample_stage_fixed_sample_size(DESIRED_SAMPLE_SIZE:int,EPSILON:float,x_obs:[[float]],s_obs:[float],PRIORS:["stats.Distribution"],x_samplers:["stats.Distribution"],model_t:Model) -> ( [[float]] , [([float],float)] ):
     """
     DESCRIPTION
     sample from parameter priors until a sufficient number of samples are close to observed parameters.
 
     PARAMETERS
-    ax (plt.Axes) - Axes to produce plot on.
     DESIRED_SAMPLE_SIZE (int) - Algorithm terminates after collection this many sample sufficient samples.
     EPSILON (int) - How close a sample must be to observations to be accepted. (Use in `uniform_kernel()`)
     ...
 
     RETURNS
-    [([float],float)] - Samples collected ([pred_var_values],response_var_value)
+    [[float]] - parameter sampels accepted
+    [([float]),float] - Observations accepted ([predictor_variables],response_variable)
     """
     SAMPLES=[]
 
@@ -216,23 +217,27 @@ def __sample_stage_fixed_sample_size(DESIRED_SAMPLE_SIZE:int,EPSILON:float,x_obs
         print("({:,}) {:,}/{:,}".format(i,len(SAMPLES),DESIRED_SAMPLE_SIZE),end="\r") # update user on sampling process
 
     print("\n")
-    return SAMPLES
 
-def __sample_stage_best_samples(NUM_RUNS:int,SAMPLE_SIZE:int,x_obs:[[float]],s_obs:[float],PRIORS:["stats.Distribution"],x_samplers:["stats.Distribution"],model_t:Model) -> [([float],float)]:
+    theta_samples=[x[0] for x in SAMPLES]
+    accepted_obs=[x[1] for x in SAMPLES]
+
+    return theta_samples,accepted_obs
+
+def __sample_stage_best_samples(NUM_RUNS:int,SAMPLE_SIZE:int,x_obs:[[float]],s_obs:[float],PRIORS:["stats.Distribution"],x_samplers:["stats.Distribution"],model_t:Model) -> ( [[float]] , [([float],float)] ):
     """
     DESCRIPTION
     make a defined number of samples and keep only the best n.
 
     PARAMETERS
-    ax (plt.Axes) - Axes to produce plot on.
     NUM_RUNS (int) - Number of samples to make
     SAMPLE_SIZE (int) - Number of samples to keep
     ...
 
     RETURNS
-    [([float],float)] - Samples collected ([pred_var_values],response_var_value)
+    [[float]] - parameter sampels accepted
+    [([float]),float] - Observations accepted ([predictor_variables],response_variable)
     """
-    SAMPLES=[None for _ in range(sample_size)]
+    SAMPLES=[None for _ in range(SAMPLE_SIZE)]
 
     # Sample-Rejection Stage
     for i in range(NUM_RUNS):
@@ -261,14 +266,74 @@ def __sample_stage_best_samples(NUM_RUNS:int,SAMPLE_SIZE:int,x_obs:[[float]],s_o
 
     # remove norm value from SAMPLES
     SAMPLES=[x[1] for x in SAMPLES]
+    theta_samples=[x[0] for x in SAMPLES]
+    accepted_obs=[x[1] for x in SAMPLES]
 
-    return SAMPLES
+    return theta_samples,accepted_obs
+
+def __sample_stage_multi_compare(NUM_RUNS:int,SAMPLE_SIZE:int,NUM_COMPARISONS:int,x_obs:[[float]],s_obs:[float],PRIORS:["stats.Distribution"],x_samplers:["stats.Distribution"],model_t:Model) -> ( [[float]] , [([float],float)] ):
+    """
+    DESCRIPTION
+    make a defined number of parameter samples and keep the best.
+    With each set of sampled parameters, multiple observations are made from the theorised model and compared to the given observations from the true model (x_obs,s_obs). The mean kernel value is used to compare the fit of each theorised model.
+
+    PARAMETERS
+    NUM_RUNS (int) - Number of samples to make
+    SAMPLE_SIZE (int) - Number of samples to keep
+    NUM_COMPARISONS (int) - Number of observations to make from theorised model (used to compare fit to true model)
+    ...
+
+
+    RETURNS
+    [[float]] - parameter sampels accepted
+    [([float]),float] - Observations accepted ([predictor_variables],response_variable)
+    """
+    SAMPLES=[None for _ in range(SAMPLE_SIZE)]
+
+    # Sample-Rejection Stage
+    for i in range(NUM_RUNS):
+        # sample parameters
+        theta_t=[pi_i.rvs(1)[0] for pi_i in PRIORS] # sample a single value from each parameter-prior
+
+        # simulate values
+        model_t.params=theta_t
+        x_ts=[[sampler.rvs(1)[0] for sampler in x_samplers] for _ in range(NUM_COMPARISONS)] # make multiple samples from theorised model
+        s_ts=[model_t.calc(x_t) for x_t in x_ts] # add response variable
+
+        # accept-reject
+        norm_vals=[[l2_norm(x_i+[s_i],x_t+[s_t]) for (x_i,s_i) in zip(x_obs,s_obs)] for (x_t,s_t) in zip(x_ts,s_ts)]
+        best_norm=[min(x) for x in norm_vals]
+        mean_norm=np.mean(best_norm)
+
+        # insert into samples
+        for j in range(max(0,SAMPLE_SIZE-i-1,SAMPLE_SIZE)):
+
+            if (SAMPLES[j]==None) or (SAMPLES[j][0]>mean_norm):
+                if (j>0): SAMPLES[j-1]=SAMPLES[j]
+                SAMPLES[j]=(mean_norm,(theta_t,list(zip(x_ts,s_ts))))
+
+        print("({:,})".format(i),end="\r") # update user on sampling process
+
+    print("\n")
+
+    # remove norm value from SAMPLES
+    SAMPLES=[x[1] for x in SAMPLES]
+    theta_samples=[x[0] for x in SAMPLES]
+
+    accepted_obs=[]
+    for x in SAMPLES:
+        for y in x[1]:
+            accepted_obs.append(y)
+    # accepted_obs=[y for y in x[1] for x in SAMPLES]
+
+    return theta_samples,accepted_obs
 
 """
    ABC
 """
 def abc_general(true_model=None,fitting_model=None,priors=None,n_obs=100,var_ranges=None,sampling_details={"sampling_method":"best_samples","sample_size":1000,"num_runs":10000}) -> Model:
     """
+    DESCRIPTION
     Approximate Bayesian Computation which terminates when a sufficient number of samples have been accepted.
     A LinearModel with two parameters is used.
 
@@ -299,8 +364,8 @@ def abc_general(true_model=None,fitting_model=None,priors=None,n_obs=100,var_ran
     print("True Model - {}\n".format(str(MODEL_STAR)))
 
     # verify inputs
-    if (var_ranges) and (len(var_ranges)!=fitting_model.n_vars): raise Exception("Incorrect number of `var_ranges` provided. (Exp={})".format(fitting_model.n_vars))
-    if (priors) and (len(priors)!=fitting_model.n_params): raise Exception("Incorrect number of `priors` provided. (Exp={})".format(fitting_model.n_params))
+    if (var_ranges) and (len(var_ranges)!=model_t.n_vars): raise Exception("Incorrect number of `var_ranges` provided. (Exp={})".format(model_t.n_vars))
+    if (priors) and (len(priors)!=model_t.n_params): raise Exception("Incorrect number of `priors` provided. (Exp={})".format(model_t.n_params))
 
     # define priors for parameters
     PRIORS=priors if (priors) else [stats.uniform(THETA_STAR[i]-8,25) for i in range(model_t.n_params)]
@@ -310,34 +375,43 @@ def abc_general(true_model=None,fitting_model=None,priors=None,n_obs=100,var_ran
     N_OBS=n_obs
 
     # generate observations from target model
-    x_samplers=[stats.uniform(r[0],r[1]) for r in VAR_RANGES]
+    x_samplers=[stats.uniform(r[0],r[1]-r[0]) for r in VAR_RANGES]
     x_obs=[[sampler.rvs(1)[0] for sampler in x_samplers] for _ in range(N_OBS)]
     if (MODEL_STAR.n_vars==1): x_obs=sorted(x_obs,key=(lambda x:x[0]))
     s_obs=[MODEL_STAR.calc(x) for x in x_obs]
 
     # perform sampling
     if (sampling_details["sampling_method"]=="best_samples"): # keep only best
-        SAMPLE_SIZE=sampling_details["sample_size"]
         NUM_RUNS=   sampling_details["num_runs"]
-        SAMPLES=__sample_stage_best_samples(NUM_RUNS,SAMPLE_SIZE,x_obs,s_obs,PRIORS,x_samplers,model_t)
+        SAMPLE_SIZE=sampling_details["sample_size"]
+
+        THETA_SAMPLES,ACCEPTED_OBS=__sample_stage_best_samples(NUM_RUNS,SAMPLE_SIZE,x_obs,s_obs,PRIORS,x_samplers,model_t)
 
     elif (sampling_details["sampling_method"]=="fixed_number"): # keep all which fulfil criteria
         SAMPLE_SIZE=sampling_details["sample_size"]
         EPSILON=    sampling_details["epsilon"]
-        SAMPLES=__sample_stage_fixed_sample_size(SAMPLE_SIZE,EPSILON,x_obs,s_obs,PRIORS,x_samplers,model_t)
+
+        THETA_SAMPLES,ACCEPTED_OBS=__sample_stage_fixed_sample_size(SAMPLE_SIZE,EPSILON,x_obs,s_obs,PRIORS,x_samplers,model_t)
+
+    elif (sampling_details["sampling_method"]=="multi_compare"):
+        NUM_RUNS       =sampling_details["num_runs"]
+        SAMPLE_SIZE    =sampling_details["sample_size"]
+        NUM_COMPARISONS=sampling_details["num_comparisons"]
+
+        THETA_SAMPLES,ACCEPTED_OBS=__sample_stage_multi_compare(NUM_RUNS,SAMPLE_SIZE,NUM_COMPARISONS,x_obs,s_obs,PRIORS,x_samplers,model_t)
 
     else:
         raise Exception("Must specify valid sampling details")
 
     # best fit model
-    theta_hat=[np.mean([s[0][i] for s in SAMPLES]) for i in range(model_t.n_params)] # posterior sample mean
+    theta_hat=[np.mean([s[i] for s in THETA_SAMPLES]) for i in range(model_t.n_params)] # posterior sample mean
     model_hat=model_t.blank_copy()
     model_hat.params=theta_hat
 
     # plot results
     observations=list(zip(x_obs,s_obs))
     fig=plt.figure()
-    __plot_results(fig,model_hat,MODEL_STAR,SAMPLES,PRIORS,VAR_RANGES,observations,plot_truth)
+    __plot_results(fig,model_hat,MODEL_STAR,THETA_SAMPLES,ACCEPTED_OBS,PRIORS,VAR_RANGES,observations,plot_truth)
 
     print("Best Model - {}".format(str(model_hat)))
 
--- Code/test.py
+++ Code/test.py
@@ -8,27 +8,60 @@ lm_4=LinearModel(4,[10,20,30,40])
 
 em=ExponentialModel([1,2],["age"])
 
-theta_star=[10,20,-1]
-f=(lambda x,theta: theta[0]+theta[1]*x[0]**2+theta[2]*(x[0]**3))
-glm=GeneralLinearModel(3,1,f,theta_star)
-
-# ABC.abc_best_samples(num_runs=10000,sample_size=1000)
-
+"""
+    FIXED NUMBER OF SAMPLES
+# fixed number of samples - EM with a GLM
+sampling_details={"sampling_method":"fixed_number","sample_size":1000,"epsilon":.1}
 theta_star=[1,1,1]; f=(lambda x,theta: theta[0]+theta[1]*x[0]+theta[2]*(x[0]**2))
 fitting_glm=GeneralLinearModel(3,1,f,theta_star)
-
 priors=[stats.uniform(0,10),stats.uniform(0,5),stats.uniform(0,5)]
-sampling_details={"sampling_method":"fixed_number","sample_size":1000,"epsilon":.1}
 
 ABC.abc_general(true_model=em,fitting_model=fitting_glm,priors=priors,var_ranges=[(-5,5)],sampling_details=sampling_details)
 
-sampling_details={"sampling_method":"best_samples","sample_size":1000,"num_runs":10000}
-ABC.abc_general(true_model=lm_2,sampling_details=sampling_details)
-
+# fixed number of samples - LM
 sampling_details={"sampling_method":"fixed_number","sample_size":1000,"epsilon":4}
 ABC.abc_general(true_model=lm_2,sampling_details=sampling_details)
-ABC.abc_general(true_model=lm_3,sampling_details=sampling_details)
-ABC.abc_general(true_model=lm_4,sampling_details=sampling_details)
 
+# fixed number of samples - GLM
+sampling_details={"sampling_method":"fixed_number","sample_size":1000,"epsilon":4}
+theta_star=[10,20,-1]; f=(lambda x,theta: theta[0]+theta[1]*x[0]**2+theta[2]*(x[0]**3))
+glm=GeneralLinearModel(3,1,f,theta_star)
 priors=[stats.uniform(5,10),stats.uniform(15,10),stats.uniform(-2,3)]
+
 ABC.abc_general(true_model=glm,priors=priors,n_obs=500,var_ranges=[(0,20)],sampling_details=sampling_details)
+"""
+
+"""
+    BEST OF SAMPLES
+# best samples for LM
+sampling_details={"sampling_method":"best_samples","sample_size":100,"num_runs":10000}
+ABC.abc_general(true_model=lm_2,sampling_details=sampling_details)
+"""
+
+"""
+    BEST OF MULTI-COMPARISON SAMPLING
+# multi-comaprisons - LM
+sampling_details={"sampling_method":"multi_compare","sample_size":10,"num_runs":1000,"num_comparisons":10}
+ABC.abc_general(true_model=lm_2,sampling_details=sampling_details)
+
+# multi-comparisons - EM
+sampling_details={"sampling_method":"multi_compare","sample_size":10,"num_runs":1000,"num_comparisons":10}
+priors=[stats.uniform(0,2),stats.uniform(1,3)]
+ABC.abc_general(true_model=em,sampling_details=sampling_details,priors=priors,var_ranges=[(-5,5)])
+
+# multi-comparison - GLM
+sampling_details={"sampling_method":"multi_compare","sample_size":10,"num_runs":1000,"num_comparisons":10}
+theta_star=[10,20,-1]; f=(lambda x,theta: theta[0]+theta[1]*x[0]**2+theta[2]*(x[0]**3))
+glm=GeneralLinearModel(3,1,f,theta_star)
+priors=[stats.uniform(5,10),stats.uniform(15,10),stats.uniform(-2,3)]
+
+ABC.abc_general(true_model=glm,priors=priors,n_obs=500,var_ranges=[(0,20)],sampling_details=sampling_details)
+
+# multi-comparison - GLM
+theta_star=[1,1/4]; f=(lambda x,theta:theta[0]*(x[0]**2)+theta[1]*(x[0]**5)) # x^2+(x^5)/4
+glm=GeneralLinearModel(2,1,f,theta_star)
+priors=[stats.uniform(0,2),stats.uniform(0,1)]
+sampling_details={"sampling_method":"multi_compare","sample_size":100,"num_runs":1000,"num_comparisons":10}
+ABC.abc_general(true_model=glm,priors=priors,var_ranges=[(-2,2)],sampling_details=sampling_details)
+
+"""
